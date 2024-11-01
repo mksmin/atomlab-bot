32dbcf4e833c32cc4e50891171c4713c3d336cb0
mks_min <mininname@vk.com>
2024-11-02 01:49:57 +0300

    Feature: Add a notification setting for starting and stopping the bot

--- config/__init__.py
+++ config/__init__.py
@@ -2,10 +2,11 @@ __all__ = (
     'get_tokens',
     'get_id_chat_root',
     'dbconf',
-    'logger'
+    'logger',
+    'BotNotification'
 )
 
 from .config import get_tokens, get_id_chat_root, logger
-from .parser_settings import dbconf
+from .parser_settings import dbconf, BotNotification
 
 
