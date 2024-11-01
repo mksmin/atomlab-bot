3b3da582b95ac5ddb96b9ced16824266d435e1c2
mks_min <mininname@vk.com>
2024-11-02 01:29:45 +0300

    Add logger to __init__.py in config

--- config/__init__.py
+++ config/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     'get_tokens',
     'get_id_chat_root',
     'dbconf',
+    'logger'
 )
 
-from .config import get_tokens, get_id_chat_root
+from .config import get_tokens, get_id_chat_root, logger
 from .parser_settings import dbconf
+
 
